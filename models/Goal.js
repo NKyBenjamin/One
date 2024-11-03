4684fbdd06e39b82107e02b2a17019a6b23edbd7
DerpShark <shark900oooo00@gmail.com>
2024-11-03 06:57:13 -0500

    update some format, added  some scripts to home.html

--- models/Goal.js
+++ models/Goal.js
@@ -2,28 +2,60 @@ const mongoose = require("mongoose");
 const dayjs = require('dayjs');
 
 const GoalSchema = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, 
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, 
         ref: "User", 
-        required: true },
-    title: {type: String, 
-        required: true },
-    description: {type: String },
-    goalType: {type: String, 
+        required: true 
+    },
+
+    title: {
+        type: String, 
+        required: true 
+    },
+
+    description: {
+        type: String 
+    },
+
+    goalType: {
+        type: String, 
         enum: ['timed', 'daily'], 
-        required: true },
-    target: {type: Number, 
-        required: true },
-    progress: {type: Number, 
-        default: 0 },
-    startDate: {type: Date, 
-        default: dayjs().toDate() },
-    endDate: {type: Date, 
-        required: true },
-    createdAt: {type: Date, 
-        default: Date.now },
-    streak: {type: Number, 
-        default: 0},
-    lastLogDate: {type: Date}
+        required: true 
+    },
+
+    target: {
+        type: Number, 
+        required: true 
+    },
+
+    progress: {
+        type: Number, 
+        default: 0 
+    },
+
+    startDate: {
+        type: Date, 
+        default: dayjs().toDate() 
+    
+    },
+    endDate: {
+        type: Date, 
+        required: true 
+    },
+
+    createdAt: {
+        type: Date, 
+        default: Date.now 
+    },
+
+    streak: {
+        type: Number, 
+        default: 0
+    },
+
+    lastLogDate: {
+        type: Date
+    }
 });
 
 module.exports = mongoose.model("Goal", GoalSchema);
